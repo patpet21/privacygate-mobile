b66359d6b688cb365d1fff3984ed8a3bf68aa91e
patpet21 <96307724+patpet21@users.noreply.github.com>
2026-09-13 17:43:42 -0400

    feat: add shared replacement mapping model

---
+++ lib/core/domain/replacement_mapping.dart
@@ -0,0 +1,11 @@
+class ReplacementMapping {
+  const ReplacementMapping({
+    required this.token,
+    required this.entityType,
+    required this.originalText,
+  });
+
+  final String token;
+  final String entityType;
+  final String originalText;
+}
