8d545358f0b3800c3f0ce71d17653fff87071a16
patpet21 <96307724+patpet21@users.noreply.github.com>
2026-09-13 17:44:42 -0400

    feat: wire mobile protect workflow into app shell

--- lib/app/privacy_gate_app.dart
+++ lib/app/privacy_gate_app.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
 
+import '../core/detection/bootstrap_pattern_detector.dart';
+import '../core/protection/privacy_gate_protector.dart';
+import '../features/protect/protect_controller.dart';
+import '../features/protect/protect_screen.dart';
+
 class PrivacyGateApp extends StatelessWidget {
   const PrivacyGateApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = ProtectController(
+      detector: const BootstrapPatternDetector(),
+      protector: const PrivacyGateProtector(),
+    );
+
     return MaterialApp(
       title: 'PrivacyGate',
-      home: const Scaffold(
-        body: Center(child: Text('PrivacyGate Mobile')),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: const Color(0xFF0B7180),
       ),
+      home: ProtectScreen(controller: controller),
     );
   }
 }
