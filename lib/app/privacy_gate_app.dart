585f9bb8aff8f369fce8e60210173ba7cd4b6c11
patpet21 <96307724+patpet21@users.noreply.github.com>
2026-09-13 17:43:31 -0400

    feat: add PrivacyGate app shell

---
+++ lib/app/privacy_gate_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PrivacyGateApp extends StatelessWidget {
+  const PrivacyGateApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PrivacyGate',
+      home: const Scaffold(
+        body: Center(child: Text('PrivacyGate Mobile')),
+      ),
+    );
+  }
+}
