fb593d589a4615302ef1791c99af44273add4eea
patpet21 <96307724+patpet21@users.noreply.github.com>
2026-09-13 21:27:41 -0400

    feat: add shared Protect Settings mobile shell

--- lib/app/privacy_gate_app.dart
+++ lib/app/privacy_gate_app.dart
@@ -2,19 +2,43 @@ import 'package:flutter/material.dart';
 
 import '../core/detection/bootstrap_pattern_detector.dart';
 import '../core/protection/privacy_gate_protector.dart';
+import '../core/settings/privacy_gate_settings.dart';
 import '../features/protect/protect_controller.dart';
 import '../features/protect/protect_screen.dart';
+import '../features/settings/settings_screen.dart';
 
-class PrivacyGateApp extends StatelessWidget {
+class PrivacyGateApp extends StatefulWidget {
   const PrivacyGateApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final controller = ProtectController(
+  State<PrivacyGateApp> createState() => _PrivacyGateAppState();
+}
+
+class _PrivacyGateAppState extends State<PrivacyGateApp> {
+  late final PrivacyGateSettings _settings;
+  late final ProtectController _protectController;
+  var _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _settings = PrivacyGateSettings();
+    _protectController = ProtectController(
       detector: const BootstrapPatternDetector(),
       protector: const PrivacyGateProtector(),
+      settings: _settings,
     );
+  }
 
+  @override
+  void dispose() {
+    _protectController.dispose();
+    _settings.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PrivacyGate',
       debugShowCheckedModeBanner: false,
@@ -22,7 +46,31 @@ class PrivacyGateApp extends StatelessWidget {
         useMaterial3: true,
         colorSchemeSeed: const Color(0xFF0B7180),
       ),
-      home: ProtectScreen(controller: controller),
+      home: Scaffold(
+        body: IndexedStack(
+          index: _selectedIndex,
+          children: [
+            ProtectScreen(controller: _protectController),
+            SettingsScreen(settings: _settings),
+          ],
+        ),
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: _selectedIndex,
+          onDestinationSelected: (index) => setState(() => _selectedIndex = index),
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Icons.shield_outlined),
+              selectedIcon: Icon(Icons.shield),
+              label: 'Protect',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.settings_outlined),
+              selectedIcon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
